d8420427bbf9978c6e6b94bc9af34b8945bf19d0
Atanu Kumar Dey <atanukumardey_2020@gift.edu.in>
2022-08-31 15:59:50 +0530

    Navigation Bar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        deepblue:"#3a488b",
+        hovercolor:"#fd7e14"
+      }
+    },
+  },
+  plugins: [],
+}
